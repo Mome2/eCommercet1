2e8eb28709de51ec8d539d2f34a0fe68aa85c7dc
Mome2 <Mome2435@yahoo.com>
2025-06-30 07:19:01 +0300

    edit setLocale middleware to check if user has defualt locale or put session locale

--- app/Http/Middleware/setLocale.php
+++ app/Http/Middleware/setLocale.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -17,8 +18,11 @@ class setLocale
      */
     public function handle(Request $request, Closure $next): Response
     {
-        if (Session::has('language')) {
-            App::setLocale(Session::get('language'));
+        if (Auth::check()) {
+            App::setLocale(Auth::user()->profle()->locale);
+        }
+        if (Session::has('locale')) {
+            App::setLocale(Session::get('locale'));
         }
 
         return $next($request);
